607979dad3418fc3524182c5324c9f8b689e0fa2
Pedro Arruda <pedro.arruda@allcomsistemas.com.br>
2024-10-29 15:57:55 -0300

    feat: initiate home page

---
+++ front/src/app/home/page.tsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <section className="container mt-10 mx-auto">
+      <div>
+        <h1 className="text-3xl font-semibold text-center">Tasks</h1>
+      </div>
+    </section>
+  );
+};
+export default Home;
